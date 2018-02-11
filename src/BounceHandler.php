38ccb703380bb4371e0e09e513e2f5d0030bfe5e
Patrick O'Connell <patrick@oconnell.id.au>
2018-02-12 08:03:16 +1100

    Fixed email matching regex
    Added support for more emails

--- src/BounceHandler.php
+++ src/BounceHandler.php
@@ -628,7 +628,8 @@ class BounceHandler {
      * @return array Returns an array of email addresses
      */
     public function findEmailAddresses ($string) {
-        preg_match_all('/[a-z0-9_\-\+]+@[a-z0-9\-]+\.([a-z]{2,3})(?:\.[a-z]{2})?/i', $string, $matches);
+        $regex = '/(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/';
+        preg_match_all($regex, $string, $matches);
         if ($matches && !empty($matches[0])) {
             return $matches[0];
         }
